bcc8dc58d6a0280ac95643f80f89b11416986b6a
estevam31 <estevamvinicius31@gmail.com>
2020-12-11 06:23:59 -0300

    feat(slides_robot): created first slide prototype

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/IBM/go-sdk-core v0.0.0-20200217212347-fe152a0e9e89
 	github.com/algorithmiaio/algorithmia-go v0.0.0-20200213204123-9e7bc5de7a2c
 	github.com/joho/godotenv v1.3.0
+	github.com/kjk/betterguid v0.0.0-20170621091430-c442874ba63a
 	github.com/sirupsen/logrus v1.7.0
 	github.com/watson-developer-cloud/go-sdk v1.7.0
 	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
--- pkg/robots/slides.go
+++ pkg/robots/slides.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/kjk/betterguid"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/slides/v1"
@@ -18,18 +19,139 @@ type SlidesRobot struct{}
 // Start SlidesRobot
 func (robot *SlidesRobot) Start(state *State) {
 	fmt.Println("[slides] => Starting...")
-	robot.createSlide(state.SearchTerm)
+	robot.createPresentation(state.SearchTerm, state.Sentences)
 	fmt.Println("[slides] => Done, Aloha :D")
 }
 
-func (robot *SlidesRobot) createSlide(title string /* sentences []*Sentence*/) *slides.Presentation {
-	fmt.Println("[slides] => Creating slide...")
+func (robot *SlidesRobot) createPresentation(title string, sentences []*Sentence) *slides.Presentation {
+	fmt.Println("[slides] => Creating presentation...")
 	slidesService := robot.createGoogleSlidesAPIService()
-	presentation, err := slidesService.Presentations.Create(&slides.Presentation{Title: title}).Do()
+
+	presentation, err := slidesService.Presentations.Create(&slides.Presentation{Title: title}).Fields("presentationId").Do()
 	if err != nil {
 		log.Fatalf("\n[slides] => Could not create presentation on google slides %v", err)
 	}
+
+	createSlidesRequests := robot.createRequestsForSlidePages(sentences, presentation.PresentationId)
+	requestBody := &slides.BatchUpdatePresentationRequest{
+		Requests: createSlidesRequests,
+	}
+	_, err = slidesService.Presentations.BatchUpdate(presentation.PresentationId, requestBody).Do()
+	if err != nil {
+		log.Fatalf("\n[slides] => Error on create slides requests %v", err)
+	}
+
+	fmt.Println("[slides] => Presentation created")
 	return presentation
+}
+
+func (robot *SlidesRobot) createRequestsForSlidePages(sentences []*Sentence, presentationID string) []*slides.Request {
+	fmt.Println("[slides] => Creating slide pages...")
+	requests := []*slides.Request{}
+	for _, sentence := range sentences {
+		slideID := "S" + betterguid.New()
+
+		createSlideBody := robot.createSlideBodyRequest(slideID, 0)
+		insertText := robot.insertTextRequests(sentence.Text, slideID, 16, 350, 350, 100)
+		insertImage := robot.insertImageRequest(sentence.Images[0], slideID, 100000.0, 100000.0)
+
+		requests = append(requests, createSlideBody)
+		requests = append(requests, insertText...)
+		requests = append(requests, insertImage)
+	}
+	return requests
+}
+
+func (robot *SlidesRobot) createSlideBodyRequest(slideID string, index int64) *slides.Request {
+	return &slides.Request{
+		CreateSlide: &slides.CreateSlideRequest{
+			ObjectId:       slideID,
+			InsertionIndex: index,
+			SlideLayoutReference: &slides.LayoutReference{
+				PredefinedLayout: "BLANK",
+			},
+		},
+	}
+}
+
+func (robot *SlidesRobot) insertTextRequests(text string, slideID string, fontSize float64, boxSize float64, translateX float64, translateY float64) []*slides.Request {
+	textBoxID := "T" + betterguid.New()
+	boxSizeDimension := slides.Dimension{
+		Magnitude: boxSize,
+		Unit:      "PT",
+	}
+	fontSizeDimension := slides.Dimension{
+		Magnitude: fontSize,
+		Unit:      "PT",
+	}
+	createTextBoxRequest := &slides.Request{
+		CreateShape: &slides.CreateShapeRequest{
+			ObjectId:  textBoxID,
+			ShapeType: "TEXT_BOX",
+			ElementProperties: &slides.PageElementProperties{
+				PageObjectId: slideID,
+				Size: &slides.Size{
+					Height: &boxSizeDimension,
+					Width:  &boxSizeDimension,
+				},
+				Transform: &slides.AffineTransform{
+					ScaleX:     1.0,
+					ScaleY:     1.0,
+					TranslateX: translateX,
+					TranslateY: translateY,
+					Unit:       "PT",
+				},
+			},
+		},
+	}
+
+	insertTextOnTextboxRequest := &slides.Request{
+		InsertText: &slides.InsertTextRequest{
+			ObjectId:       textBoxID,
+			InsertionIndex: 0,
+			Text:           text,
+		},
+	}
+	updateTextStyleRequest := &slides.Request{
+		UpdateTextStyle: &slides.UpdateTextStyleRequest{
+			ObjectId: textBoxID,
+			Style: &slides.TextStyle{
+				FontSize:   &fontSizeDimension,
+				FontFamily: "Montserrat",
+			},
+			Fields: "fontSize, fontFamily",
+		},
+	}
+
+	return []*slides.Request{createTextBoxRequest, insertTextOnTextboxRequest, updateTextStyleRequest}
+}
+
+func (robot *SlidesRobot) insertImageRequest(imageURL string, slideID string, translateX float64, translateY float64) *slides.Request {
+	imageID := "I" + betterguid.New()
+	dimension := slides.Dimension{
+		Magnitude: 4000000,
+		Unit:      "EMU",
+	}
+	return &slides.Request{
+		CreateImage: &slides.CreateImageRequest{
+			ObjectId: imageID,
+			Url:      imageURL,
+			ElementProperties: &slides.PageElementProperties{
+				PageObjectId: slideID,
+				Size: &slides.Size{
+					Height: &dimension,
+					Width:  &dimension,
+				},
+				Transform: &slides.AffineTransform{
+					ScaleX:     1.0,
+					ScaleY:     1.0,
+					TranslateX: translateX,
+					TranslateY: translateY,
+					Unit:       "EMU",
+				},
+			},
+		},
+	}
 }
 
 func (robot *SlidesRobot) createGoogleSlidesAPIService() *slides.Service {
